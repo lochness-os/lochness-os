ce50352917ff071f3c880542441dcd0811c7c2f3
Michael Brockus <55331536+michaelbrockus@users.noreply.github.com>
2021-01-13 08:33:59 -0800

    Create run.sh

---
+++ scripts/run.sh
@@ -0,0 +1,33 @@
+if [ -z $LOCHNESS ]; then
+	export LOCHNESS=$(dirname $(readlink -f "$0"))/..
+fi
+
+qemu(){
+	qemu-system-x86_64 --enable-kvm $LOCHNESS/Disks/LochNess.vhd -no-reboot -no-shutdown -m 512M -device qemu-xhci -M q35 -smp 2 -serial stdio -netdev user,id=net0 -device e1000,netdev=net0,mac=DE:AD:69:BE:EF:42
+}
+
+qemuefi(){
+	qemu-system-x86_64 --bios /usr/share/edk2-ovmf/x64/OVMF.fd -M q35 -m 512M -drive file=Disks/LochNess.vhd,id=nvme0 -device nvme,serial=deadbeef69,id=nvme0 -serial stdio
+}
+
+qemuusb(){
+	qemu-system-x86_64 --enable-kvm -drive if=none,id=usbd,file=$LOCHNESS/Disks/LochNess.vhd -no-reboot -no-shutdown -m 512M -device qemu-xhci,id=xhci -device usb-storage,bus=xhci.0,drive=usbd -M q35 -smp 2 -serial stdio -netdev user,id=net0 -device e1000,netdev=net0,mac=DE:AD:69:BE:EF:42
+}
+
+
+vbox(){
+	VBoxManage startvm "LochNess"
+}
+
+debug_qemu(){
+	echo "target remote localhost:1234" > $(dirname $(readlink -f "$0"))/debug.gdb
+	echo "symbol-file $LOCHNESS/kernel/build/kernel.sys" >> $(dirname $(readlink -f "$0"))/debug.gdb
+	qemu-system-x86_64 -s -S Disks/LochNess.vhd -no-reboot -no-shutdown --enable-kvm -chardev stdio,id=gdb0 -device isa-debugcon,iobase=0x402,chardev=gdb0,id=d1 -d int -m 512M -M q35 -smp 2 -netdev user,id=net0 -device e1000,netdev=net0,mac=DE:AD:69:BE:EF:42&
+	gdb -x $(dirname $(readlink -f "$0"))/debug.gdb
+}
+
+if [ -z $1 ]; then
+	qemu
+else
+	$1
+fi
