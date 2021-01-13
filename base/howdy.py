bd4bc352de9fe0ab9af8964231e02f02688033ec
Michael Brockus <55331536+michaelbrockus@users.noreply.github.com>
2021-01-13 07:37:21 -0800

    Create howdy.py

---
+++ base/howdy.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+#
+# file: howdy.py
+# author: Michael Brockus
+# gmail: <michaelbrockus@gmail.com>
+#
+import platform
+
+print("Hello, " + platform.system() + " World!")
